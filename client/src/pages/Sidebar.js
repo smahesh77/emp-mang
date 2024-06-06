a9b7cc52cb5907bca9909bf0fcf15d737ec885a7
smahesh77 <smahesh7736@gmail.com>
2024-06-07 03:20:27 +0530

    test

---
+++ client/src/pages/Sidebar.js
@@ -0,0 +1,18 @@
+// Sidebar.js
+
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <aside className="sidebar">
+      <h2>Sidebar</h2>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
